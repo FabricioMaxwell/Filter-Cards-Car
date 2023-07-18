8cb5378353d3dabb19bebdbf293fc866079b326d
Fabrício Maxwell <102274424+FabricioMaxwell@users.noreply.github.com>
2023-07-18 10:01:36 -0300

    update

--- script.js
+++ script.js
@@ -1,4 +1,30 @@
+const btns = document.querySelectorAll(".btn");
+const filter = document.getElementById("container-filters")
 
-function filterSelection() {
-    filterCards.style.display = "grid";
+btns.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        const category = btn.dataset.category;
+
+        removeActiveClasses()
+        btn.classList.add("active");
+
+        filters(category)
+    })
+})
+
+
+function removeActiveClasses() {
+    btns.forEach((btn) => {
+        btn.classList.remove("active");
+    })
+}
+
+function filters(category) {
+    filter.querySelectorAll(".filterCard").forEach((filters1) => {
+        if (category === "all" || filters1.dataset.category === category) {
+            filters1.style.display = "flex"
+        } else {
+            filters1.style.display = "none"
+        }
+    })
 }
